3f657228c18b8bfec24e5899eb4806bab2d51d02
langamitch <langamitch69@gmail.com>
2025-07-13 18:24:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,22 +63,18 @@ function listenForPosts() {
           <div class="content">${post.suggestion || ""}</div>
 
           <div class="post-actions">
-            <div class="action-group">
-              <button class="action-btn" onclick="likePost(this, '${postId}')" aria-label="Like">
-                <span class="material-symbols-outlined">
-favorite
-</span>
-              </button>
-              <button class="action-btn" onclick="dislikePost(this, '${postId}')" aria-label="Dislike">
-                <span class="material-symbols-outlined">
-heart_minus
-</span>
-              </button>
-            </div>
+            <button class="action-btn" onclick="likePost(this, '${postId}')" aria-label="Like">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
+                <path d="M0 0h24v24H0V0z" fill="none"/>
+                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+              </svg>
+            </button>
+            
             <button class="action-btn save-btn" onclick="savePost(this, '${postId}')" aria-label="Save">
-    <span class="material-symbols-outlined">
-bookmark
-</span>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
+                <path d="M0 0h24v24H0V0z" fill="none"/>
+                <path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z"/>
+              </svg>
             </button>
           </div>
         `;
@@ -91,6 +87,7 @@ bookmark
     }
   );
 }
+
 
 
 
