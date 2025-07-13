e335bf09df1671c866432e165adef41425576382
langamitch <langamitch69@gmail.com>
2025-07-13 20:04:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,9 +59,8 @@ function listenForPosts() {
         postCard.innerHTML = `
           <div class="top" onclick="window.open('${post.socialLink || '#'}', '_blank')">
             <div class="profilepic"></div>
-            <div class="profile">${post.name || "Unknown"} <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>
+            <div class="profile">${post.name || "Unknown"} <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m560-240-56-58 142-142H160v-80h486L502-662l58-58 240 240-240 240Z"/></svg>
 
-      
        </div>
           </div> 
   
